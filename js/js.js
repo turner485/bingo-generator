f0556350c39568ef9e629902e1ce241be4c59f5c
Benjamin <turner485@hotmail.co.uk>
2019-04-05 14:23:19 +0100

    solved bingo thing yoo

--- js/js.js
+++ js/js.js
@@ -1,28 +1,34 @@
 // Generate an Array with 90 indexes (0-89)
-// let arr = Array.from(Array(90).keys())
+let arr = [];
 
-
-function rollDice() {
-  return new Promise((resolve, reject) => {
-    var duration = 3000;
-    var output = $("#change");
-    var started = new Date().getTime();
-    
-    const intervalId = setInterval(() => {
-        let formattedNumber = ("0" + Math.floor(Math.random() * 90 + 1)).slice(-2);
-
-        if ( new Date().getTime() - started < duration ) {
-            output.text(formattedNumber);  // Changes dom text
-        } else {
-            clearInterval(intervalId);  // Stops the interval
-            output.text(formattedNumber);  // Changes dom text
-            resolve(formattedNumber);
-        }
-    }, 100)
-  })
+for (let i = 1; i < 91; i++) {
+    arr.push(i); //Example, pushing 5 integers in an array
 }
 
-function num() {
-    rollDice()
-    .then(value => console.log(`${value}`))
+function bingo() {
+    return new Promise((resolve, reject) => {
+        let duration = 100;
+        let output = $("#change");
+        let started = new Date().getTime();
+
+        const intervalId = setInterval(() => {
+            let key = Math.floor(Math.random() * arr.length);
+            let formattedNumber = ("0" + arr[key]).slice(-2);
+            let magicNumber = output.text(formattedNumber);
+            if (new Date().getTime() - started < duration) {
+                magicNumber;
+            } else {
+                clearInterval(intervalId); // Stops the interval
+                formattedNumber;
+                let index = arr.indexOf(arr[key]);
+                arr.splice(index, 1);
+                console.log(arr);
+                // $(".contain").append("<p>" + formattedNumber + "</p>");
+				$(".cell[data-cell='"+formattedNumber+"']").text(formattedNumber);
+
+            }
+        }, 100);
+    });
 }
+
+console.log(arr);
